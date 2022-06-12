3a034df4282b22bbdfb601b2a7585c4a1cea4f2e
hainguyen201 <haivnguyen1999@gmail.com>
2022-06-13 00:33:44 +0700

    update kafka consumer

--- code/Dynamic-Kafka-Consumer/src/main/java/com/faza/example/dynamickafkaconsumer/ManualConsumer.java
+++ code/Dynamic-Kafka-Consumer/src/main/java/com/faza/example/dynamickafkaconsumer/ManualConsumer.java
@@ -12,38 +12,38 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class ManualConsumer {
 
-    public static void main(String[] args) throws Exception {
-
-        String topicName = "SupplierTopic";
-        String groupName = "SupplierTopicGroup";
-
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", groupName);
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("enable.auto.commit", "false");
-
-        KafkaConsumer<String, String> consumer = null;
-
-        try {
-            consumer = new KafkaConsumer<>(props);
-            consumer.subscribe(Arrays.asList(topicName));
-
-            while (true) {
-                Duration duration= Duration.ofSeconds(10);
-                ConsumerRecords<String, String> records = consumer.poll(duration);
-                for (ConsumerRecord<String, String> record : records) {
-                    System.out.println("Supplier id= " + String.valueOf(record.value()));
-                }
-                consumer.commitAsync();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            consumer.commitSync();
-            consumer.close();
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//
+//        String topicName = "SupplierTopic";
+//        String groupName = "SupplierTopicGroup";
+//
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", "localhost:9092");
+//        props.put("group.id", groupName);
+//        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put("enable.auto.commit", "false");
+//
+//        KafkaConsumer<String, String> consumer = null;
+//
+//        try {
+//            consumer = new KafkaConsumer<>(props);
+//            consumer.subscribe(Arrays.asList(topicName));
+//
+//            while (true) {
+//                Duration duration= Duration.ofSeconds(10);
+//                ConsumerRecords<String, String> records = consumer.poll(duration);
+//                for (ConsumerRecord<String, String> record : records) {
+//                    System.out.println("Supplier id= " + String.valueOf(record.value()));
+//                }
+//                consumer.commitAsync();
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        } finally {
+//            consumer.commitSync();
+//            consumer.close();
+//        }
+//    }
 
 }
